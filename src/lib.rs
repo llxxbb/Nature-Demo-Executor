a9f622839cff8eec0a81b635daba01c86f829ca3
llxxbb <llxxbb@yeah.net>
2019-10-06 10:57:43 +0800

    rename CallOutParameter to ConverterParameter

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use nature_common::{CallOutParameter, ConverterReturned, Instance};
+use nature_common::{ConverterParameter, ConverterReturned, Instance};
 
 #[no_mangle]
 #[allow(unused_attributes)]
-pub extern fn order_new(_para: &CallOutParameter) -> ConverterReturned {
+pub extern fn order_new(_para: &ConverterParameter) -> ConverterReturned {
     let mut instance = Instance::default();
     instance.data.content = "".to_string();
     ConverterReturned::Instances(vec![instance])
