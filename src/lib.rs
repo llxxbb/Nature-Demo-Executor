8c270139fb48258deb6f5bbbace9be45219560da
llxxbb <llxxbb@yeah.net>
2019-10-07 17:42:13 +0800

    rename status to state

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+extern crate serde_json;
+
 use nature_common::{ConverterParameter, ConverterReturned, Instance};
+use nature_demo_common::{Order, OrderAccount, OrderAccountReason, Payment};
 
 #[no_mangle]
 #[allow(unused_attributes)]
@@ -10,16 +13,41 @@ pub extern fn order_new(_para: &ConverterParameter) -> ConverterReturned {
 
 #[no_mangle]
 #[allow(unused_attributes)]
-pub extern fn order_receivable(_para: &ConverterParameter) -> ConverterReturned {
+pub extern fn order_receivable(para: &ConverterParameter) -> ConverterReturned {
+    let order: Order = serde_json::from_str(&para.from.content).unwrap();
+    let oa = OrderAccount {
+        receivable: order.price,
+        total_paid: 0,
+        last_paid: 0,
+        reason: OrderAccountReason::NewOrder,
+        diff: 0 - order.price as i32,
+    };
     let mut instance = Instance::default();
-    instance.data.content = "".to_string();
+    instance.content = serde_json::to_string(&oa).unwrap();
+    ConverterReturned::Instances(vec![instance])
+}
+
+#[no_mangle]
+#[allow(unused_attributes)]
+pub extern fn pay_count(para: &ConverterParameter) -> ConverterReturned {
+    let payment: Payment = serde_json::from_str(&para.from.content).unwrap();
+    let old = para.last_state.as_ref().unwrap();
+    let mut oa: OrderAccount = serde_json::from_str(&old.content).unwrap();
+    oa.total_paid += payment.paid;
+    oa.diff = oa.total_paid as i32 - oa.receivable as i32;
+    if oa.diff > 0 {
+        oa.total_paid = oa.receivable;
+    }
+    oa.last_paid = payment.paid;
+    oa.reason = OrderAccountReason::Pay;
+    let mut instance = Instance::default();
+    instance.content = serde_json::to_string(&oa).unwrap();
     ConverterReturned::Instances(vec![instance])
 }
 
 #[no_mangle]
 #[allow(unused_attributes)]
 pub extern fn order_paid(_para: &ConverterParameter) -> ConverterReturned {
-    let mut instance = Instance::default();
-    instance.data.content = "".to_string();
-    ConverterReturned::Instances(vec![instance])
+    // TODO
+    unimplemented!()
 }
