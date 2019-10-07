698a2a1102111656da46d3a811e798cbde6f9e12
llxxbb <llxxbb@yeah.net>
2019-10-07 18:28:18 +0800

    meta setting is_empty_content support

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,6 @@ use nature_demo_common::{Order, OrderAccount, OrderAccountReason, Payment};
 
 #[no_mangle]
 #[allow(unused_attributes)]
-pub extern fn order_new(_para: &ConverterParameter) -> ConverterReturned {
-    let mut instance = Instance::default();
-    instance.data.content = "".to_string();
-    ConverterReturned::Instances(vec![instance])
-}
-
-#[no_mangle]
-#[allow(unused_attributes)]
 pub extern fn order_receivable(para: &ConverterParameter) -> ConverterReturned {
     let order: Order = serde_json::from_str(&para.from.content).unwrap();
     let oa = OrderAccount {
@@ -43,11 +35,4 @@ pub extern fn pay_count(para: &ConverterParameter) -> ConverterReturned {
     let mut instance = Instance::default();
     instance.content = serde_json::to_string(&oa).unwrap();
     ConverterReturned::Instances(vec![instance])
-}
-
-#[no_mangle]
-#[allow(unused_attributes)]
-pub extern fn order_paid(_para: &ConverterParameter) -> ConverterReturned {
-    // TODO
-    unimplemented!()
 }
