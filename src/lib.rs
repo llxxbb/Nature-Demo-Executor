8354b9f8f2791c6dca485ba47250b27b3778a9b8
llxxbb <llxxbb@yeah.net>
2019-10-07 11:10:45 +0800

    abstract util

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,19 @@ pub extern fn order_new(_para: &ConverterParameter) -> ConverterReturned {
     instance.data.content = "".to_string();
     ConverterReturned::Instances(vec![instance])
 }
+
+#[no_mangle]
+#[allow(unused_attributes)]
+pub extern fn order_receivable(_para: &ConverterParameter) -> ConverterReturned {
+    let mut instance = Instance::default();
+    instance.data.content = "".to_string();
+    ConverterReturned::Instances(vec![instance])
+}
+
+#[no_mangle]
+#[allow(unused_attributes)]
+pub extern fn order_paid(_para: &ConverterParameter) -> ConverterReturned {
+    let mut instance = Instance::default();
+    instance.data.content = "".to_string();
+    ConverterReturned::Instances(vec![instance])
+}
