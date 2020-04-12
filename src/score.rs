d1d6d64005aa10e2705ffbf332ccd1f2ed231792
llxxbb <llxxbb@yeah.net>
2020-04-12 22:33:55 +0800

    fix bug: local filter crashed.

--- src/score.rs
+++ src/score.rs
@@ -1,5 +1,6 @@
 use nature_common::{Instance, NatureError, Result};
 
+
 #[no_mangle]
 #[allow(unused_attributes)]
 #[allow(improper_ctypes)]
@@ -7,19 +8,25 @@ pub extern fn person_score_filter(para: &Vec<Instance>) -> Result<Vec<Instance>>
     let mut rtn: Vec<Instance> = vec![];
     for input in para {
         let mut one = input.clone();
-        let kv = serde_json::from_str::<Vec<KV>>(&one.para)?;
+        let kv = serde_json::from_str::<Vec<KV>>(&input.content);
+        let kv = match kv {
+            Ok(kv) => kv,
+            Err(err) => {
+                return Err(NatureError::LogicalError(err.to_string()));
+            }
+        };
         if kv.len() != 1 {
             return Err(NatureError::LogicalError("should return one item".to_owned()));
         }
-        one.para = kv[0].value.to_string();
+        one.content = kv[0].value.to_string();
         rtn.push(one);
     }
     Ok(rtn)
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Debug, Default)]
 struct KV {
     #[allow(dead_code)]
-    key: String,
-    value: i32,
+    pub key: String,
+    pub value: i32,
 }
